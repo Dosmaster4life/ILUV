1bf0d8beb4e40de08d84fb7eb309780a889b1c0e
dosmaster4life <jefferysuperman@gmail.com>
2022-06-24 16:44:56 -0600

    Code Reformatting

---
+++ lib/screens/CreateKioskInteractor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CreateKioskInteractor extends StatefulWidget {
+  const CreateKioskInteractor({Key? key}) : super(key: key);
+
+  @override
+  State<CreateKioskInteractor> createState() => _CreateKioskInteractorState();
+}
+
+class _CreateKioskInteractorState extends State<CreateKioskInteractor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

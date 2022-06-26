e553020cf9b37b84fe8cf21dcf7d5e50bbd0989c
dosmaster4life <jefferysuperman@gmail.com>
2022-06-26 15:07:30 -0600

    Items are editable

--- lib/Widgets/CreationForm.dart
+++ lib/Widgets/CreationForm.dart
@@ -1,12 +1,16 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/Home.dart';
+import 'AppBars/AppBars.dart';
 
 class CreationForm extends StatefulWidget {
-  const CreationForm({Key? key}) : super(key: key);
+  const CreationForm({Key? key, required this.documentExistString}) : super(key: key);
+  final String documentExistString;
 
   @override
   State<CreationForm> createState() => _CreationFormState();
@@ -19,9 +23,17 @@ class _CreationFormState extends State<CreationForm> {
   final urlController = TextEditingController();
   
   Future<void> saveItem() async {
-    DocumentReference createPost = FirebaseFirestore.instance
-        .collection(FirebaseAuth.instance.currentUser!.uid)
-        .doc();
+    DocumentReference createPost;
+    if(widget.documentExistString != "") {
+      createPost = FirebaseFirestore.instance
+          .collection(FirebaseAuth.instance.currentUser!.uid)
+          .doc();
+    }else {
+      createPost = FirebaseFirestore.instance
+          .collection(FirebaseAuth.instance.currentUser!.uid)
+          .doc(widget.documentExistString);
+    }
+
 
     return createPost.set({
       'URL': urlController.text,
@@ -37,56 +49,82 @@ class _CreationFormState extends State<CreationForm> {
   }
 
 
-  Widget build(BuildContext context) {
 
-    return SafeArea(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Expanded(
-            child: Column(
-          children: [
-            Expanded(flex: 1, child: Container()),
 
-            Expanded(flex: 3, child: Container()),
-            ElevatedButton.icon(
-              onPressed: () => {
-                saveItem(),
-              },
-              icon: Icon(Icons.save),
-              label: Text("Save"),
-            ),
-          ],
-        )),
-        Expanded(
-            child: Column(
-          children: [
-            Text("Title"),
-            TextField(
-              controller: titleController,
-            ),
-            Text("Description"),
-            TextFormField(
-              controller: descriptionController,
-              keyboardType: TextInputType.text,
-              maxLines: 5,
-            ),
-            Text("Youtube Link"),
-            TextField(
-              controller: urlController,
-            ),
-            Expanded(flex: 3, child: Container()),
-            ElevatedButton.icon(
-              onPressed: () => {
-    MaterialPageRoute(
-    builder: (context) => const Home())
-              },
-              icon: Icon(Icons.cancel),
-              label: Text("Cancel"),
-            ),
-          ],
-        ))
-      ],
-    ));
+  void getDocumentData() {
+    DocumentReference doc = FirebaseFirestore.instance.collection(FirebaseAuth.instance.currentUser!.uid).doc(widget.documentExistString);
+    doc.get()
+        .then((DocumentSnapshot ds) {
+      if (ds.exists) {
+        titleController.text = ds["Title"];
+        descriptionController.text = ds["Description"];
+        urlController.text = ds["URL"];
+      }
+    });
   }
+  Widget build(BuildContext context) {
+    if (widget.documentExistString != "") {
+      getDocumentData();
+    }
+
+    return  Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBars(
+        ID: 3,
+        title: "Add Video",),
+    body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(flex: 1, child: Container()),
+
+                      Expanded(flex: 3, child: Container()),
+                      ElevatedButton.icon(
+                        onPressed: () =>
+                        {
+                          saveItem(),
+                        },
+                        icon: Icon(Icons.save),
+                        label: Text("Save"),
+                      ),
+                    ],
+                  )),
+              Expanded(
+                  child: Column(
+                    children: [
+                      Text("Title"),
+                      TextField(
+                        controller: titleController,
+                      ),
+                      Text("Description"),
+                      TextFormField(
+                        controller: descriptionController,
+                        keyboardType: TextInputType.text,
+                        maxLines: 5,
+                      ),
+                      Text("Youtube Link"),
+                      TextField(
+                        controller: urlController,
+                      ),
+                      Expanded(flex: 3, child: Container()),
+                      ElevatedButton.icon(
+                        onPressed: () =>
+                        {
+                          MaterialPageRoute(
+                              builder: (context) => const Home())
+                        },
+                        icon: Icon(Icons.cancel),
+                        label: Text("Cancel"),
+                      ),
+                    ],
+                  ))
+            ],
+          )));
+
+  }
+
+
 }
--- lib/screens/HomeView.dart
+++ lib/screens/HomeView.dart
@@ -2,9 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iluv/Widgets/CreationForm.dart';
 
 import '../Widgets/AppBars/AppBars.dart';
-import 'CreateItem.dart';
+
 
 class Screen1 extends StatefulWidget {
   const Screen1({Key? key}) : super(key: key);
@@ -32,7 +33,10 @@ class _Screen1State extends State<Screen1> {
             return Card(
                 child: ListTile(
                   onTap: () async {
-
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>  CreationForm(documentExistString: document.id?? "")),
+                    );
                   },
                   title: new Text(document["Title"]),
                 ));
@@ -51,12 +55,12 @@ class _Screen1State extends State<Screen1> {
         onPressed: () {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => const CreateItem()),
+          MaterialPageRoute(builder: (context) => const CreationForm(documentExistString: "")),
         );
       },
 
       ),
-      appBar: AppBars(ID: 0, title: "Home"),
+      appBar: AppBars(ID: 0, title: "Video List"),
 
     );
   }

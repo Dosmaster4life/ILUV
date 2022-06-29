e1da6a3e8c2954911e57070aadaeeea30c8c3641
dosmaster4life <jefferysuperman@gmail.com>
2022-06-29 11:10:45 -0600

    Hide debug banner

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -22,6 +22,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner:false,
       theme: ThemeData(
         // Define the default brightness and colors.
         brightness: themeBrightness,
